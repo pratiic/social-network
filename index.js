1d2274e91bd4efeeadc42cb33f853c7c22019d0e
pratik bhandari <pratikbhandari9292@gmail.com>
2021-05-23 20:22:12 +0545

    fix a small issue

--- index.js
+++ index.js
@@ -185,7 +185,7 @@ if (process.env.NODE_ENV === "production") {
 
 	app.get("*", (request, response) => {
 		response.sendFile(
-			path.resolve(__dirname, "client", "build", "index..html")
+			path.resolve(__dirname, "client", "build", "index.html")
 		);
 	});
 }
